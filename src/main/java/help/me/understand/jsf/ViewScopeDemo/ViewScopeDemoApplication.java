2829f8614f18b162cf7d04115a3c5afced56f1bc
jason.norman <jason.norman@quikq.com>
2019-05-28 10:24:59 -0500

    First commit.

---
+++ src/main/java/help/me/understand/jsf/ViewScopeDemo/ViewScopeDemoApplication.java
@@ -0,0 +1,13 @@
+package help.me.understand.jsf.ViewScopeDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewScopeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViewScopeDemoApplication.class, args);
+	}
+
+}
